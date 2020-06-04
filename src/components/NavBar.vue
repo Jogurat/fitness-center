9583a22b5a360fbda101ab09d518d1ed86693dc9
Petar Ostojic <pera.ostojic@gmail.com>
2020-06-04 12:57:29 +0200

    Added language icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="nav">
-    <div class="lang-toggle">
-      <button @click="changeLang">{{ lang }}</button>
-    </div>
     <router-link class="nav-link" to="/">{{ link1[lang]}}</router-link>
     <div class="dropdown nav-link">{{ link2[lang]}}</div>
     <router-link class="nav-link" to="/">{{ link3[lang]}}</router-link>
     <router-link class="nav-link" to="/">{{ link4[lang]}}</router-link>
+    <div class="lang-toggle-container">
+      <button class="lang-toggle nav-link" @click="changeLang">{{ lang }}</button>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-globe lang-toggle-icon"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <line x1="2" y1="12" x2="22" y2="12" />
+        <path
+          d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"
+        />
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -79,5 +97,24 @@ export default {
 
 .dropdown {
   cursor: pointer;
+}
+
+.lang-toggle-container {
+  display: flex;
+}
+
+.lang-toggle {
+  background: none;
+  background-color: rgba(0, 0, 0, 0);
+  cursor: pointer;
+  border: none;
+}
+
+.lang-toggle:focus {
+  outline: none;
+}
+
+.lang-toggle-icon {
+  color: white;
 }
 </style>
